47be94e3af8b3d0df9f9f0b9b403a7ffd2dca2e4
eunoia-code <rsh.newarray@gmail.com>
2022-09-07 13:21:59 +0800

    init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import '../public/css/animate.css'
+import '../public/css/tiny-slider.css'
+import '../public/fonts/lineicons/font-css/LineIcons.css'
+import '../public/css/tailwindcss.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
